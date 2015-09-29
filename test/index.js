c253cb918ed4381407e34eed5de38c2128797aaa
Macklin Underdown <macklin@detroitlabs.com>
2015-09-28 22:08:28 -0400

    ensure options parameter contains valid values

--- test/index.js
+++ test/index.js
@@ -1,8 +1,81 @@
-import assert from 'assert';
-import hipstat from '../lib';
+import chai from 'chai';
+let expect = chai.expect;
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+import Hipchatter from 'hipchatter';
+import HipStat from '../lib/';
 
-describe('hipstat', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+describe('HipStat', () => {
+
+  var viewUserStub;
+  var updateUserStub;
+
+  beforeEach(() => {
+    viewUserStub = sinon.stub(Hipchatter.prototype, 'view_user');
+    updateUserStub = sinon.stub(Hipchatter.prototype, 'update_user');
   });
+
+  afterEach(() => {
+    viewUserStub.restore();
+    updateUserStub.restore();
+  });
+
+  describe('options', () => {
+
+    it('should throw when options are undefined', () => {
+      expect(() => {
+        new HipStat().user(null, () => {});
+      }).to.throw(/Must pass in options.status but was undefined/);
+    });
+
+    describe('.status', () => {
+
+      it('should throw when undefined', () => {
+        expect(() => {
+          new HipStat().user({}, () => {});
+        }).to.throw(/Must pass in options.status but was undefined/);
+      });
+
+      it('should not throw when status is an empty String', () => {
+        expect(() => {
+          new HipStat().user({status: '', show: 'chat'}, () => {});
+        }).not.to.throw();
+      });
+
+    });
+
+    describe('.show', () => {
+
+      it('should throw when undefined', () => {
+        expect(() => {
+          new HipStat().user({ status: 'test status' }, () => {});
+        }).to.throw(/Must pass in options.show but was undefined/);
+      });
+
+      it('should throw when invalid', () => {
+        let options = {
+          status: 'test status',
+          show: 'back'
+        };
+        expect(() => {
+          new HipStat().user(options, () => {});
+        }).to.throw(/Invalid option 'back' for options.show/);
+      });
+
+      ['chat', 'away', 'dnd', 'xa'].forEach((item) => {
+        it(`${item} should be a valid value`, () => {
+          let options = {
+            status: 'test',
+            show: item
+          };
+          expect(() => {
+            new HipStat().user(options, () => {});
+          }).not.to.throw();
+        });
+      });
+
+    });
+  });
+
 });
